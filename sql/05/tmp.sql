204d5ff91e3bd7ec22d186ba8b78d94dcc44a4e1
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-11-13 02:54:49 +0900

    集約関数

--- sql/05/tmp.sql
+++ sql/05/tmp.sql
@@ -1,14 +1,19 @@
-START TRANSACTION;
+SELECT
+    MIN(level) AS "min_lv.",
+    MAX(level) AS "max_lv.",
+    MIN(name) AS "min_name",
+    MAX(name) AS "max_name"
+FROM
+    s_characters;
 
-UPDATE s_characters
-SET
-    buff = 0.3
-WHERE
-    level < 40
-RETURNING -- ◀ ここに注目
-    id,
-    name,
-    level,
-    buff;
+SELECT
+    MIN(created_on) AS "min_created_on",
+    MAX(created_on) AS "max_created_on"
+FROM
+    s_characters;
 
-ROLLBACK;
+SELECT
+    MIN(LENGTH(name)) AS "min_len_name",
+    MAX(LENGTH(name)) AS "max_len_name"
+FROM
+    s_characters;
