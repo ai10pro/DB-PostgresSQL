d7f37137693421df611ec8018ef1e4b4c958fa59
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-11-19 18:57:18 +0900

    create 1 file and update 1 file

--- sql/05/tmp.sql
+++ sql/05/tmp.sql
@@ -1,14 +1,31 @@
 SELECT
     CASE
-        WHEN GROUPING(guild) = 1 THEN '(Total)'
-        WHEN guild IS NULL THEN '(Freelancer)'
-        ELSE guild
-    END AS "guild",
-    ROUND(AVG(level), 1) AS "avg_lv",
+        WHEN last_login_at >= DATE '2025-07-01' AND
+        last_login_at < DATE '2025-08-01' THEN '2025/07'
+        WHEN last_login_at >= DATE '2025-08-01' AND
+        last_login_at < DATE '2025-09-01' THEN '2025/08'
+        WHEN last_login_at >= DATE '2025-09-01' AND
+        last_login_at < DATE '2025-10-01' THEN '2025/09'
+        WHEN last_login_at >= DATE '2025-10-01' AND
+        last_login_at < DATE '2025-11-01' THEN '2025/10'
+        ELSE '(その他)'
+    END AS "date",
     COUNT(*) AS "num"
 FROM
     s_characters
+WHERE
+    last_login_at IS NOT NULL
 GROUP BY
-    ROLLUP (guild)
+    CASE
+        WHEN last_login_at >= DATE '2025-07-01' AND
+        last_login_at < DATE '2025-08-01' THEN '2025/07'
+        WHEN last_login_at >= DATE '2025-08-01' AND
+        last_login_at < DATE '2025-09-01' THEN '2025/08'
+        WHEN last_login_at >= DATE '2025-09-01' AND
+        last_login_at < DATE '2025-10-01' THEN '2025/09'
+        WHEN last_login_at >= DATE '2025-10-01' AND
+        last_login_at < DATE '2025-11-01' THEN '2025/10'
+        ELSE '(その他)'
+    END
 ORDER BY
-    guild;
+    "date";
